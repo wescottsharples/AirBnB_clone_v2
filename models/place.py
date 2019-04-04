336077fdc31764a0f4fe3891593087a3dcbb11ac
truong21 <543@holbertonschool.com>
2019-04-04 00:06:58 +0000

    fixed foreignkey

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the place class"""
 import os
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel
@@ -25,8 +25,8 @@ class Place(BaseModel, Base):
     __tablename__ = "places"
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
-        user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)

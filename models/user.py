9fb9cd335e0afff9e9b378d5238b67a4eeb328b0
wescottsharples <wesc@ttsharpl.es>
2019-04-04 00:55:35 +0000

    Fix nullability of names

--- models/user.py
+++ models/user.py
@@ -19,8 +19,8 @@ class User(BaseModel, Base):
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user",
                               cascade="all, delete-orphan")
     else:

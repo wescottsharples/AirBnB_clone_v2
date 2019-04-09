0e78f865926c8281bc17d4f8f52f3c00d9fbe790
wescottsharples <wesc@ttsharpl.es>
2019-04-09 02:09:31 +0000

    Fix inclusion of extra '/' in path

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,6 +19,7 @@ def do_pack():
     except:
         return None
 
+
 def do_deploy(archive_path):
     """distributes an archive to my web servers"""
 
@@ -30,12 +31,12 @@ def do_deploy(archive_path):
         name = filename.split('.')[0]
         path = "/data/web_static/releases/{}/".format(name)
 
-        run("mkdir -p {}".format(path))
         put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path))
         run("tar -xzf /tmp/{} -C {}".format(filename, path))
         run("rm /tmp/{}".format(filename))
-        run("mv {}/web_static/* {}".format(path, path))
-        run("rm -rf {}/web_static".format(path))
+        run("mv {}web_static/* {}".format(path, path))
+        run("rm -rf {}web_static".format(path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current")
         return True

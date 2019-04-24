a94295943a59c92f0f80d25d81f735386fbb5f7b
Wescott Sharples <wesc@ttsharpl.es>
2019-04-24 12:38:09 -0700

    Script defaults text value to 'is cool'

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Creates and runs a Flask web app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def py_text(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

462721eba7690070e40cd737e790cc8ffcd3412d
Wescott Sharples <wesc@ttsharpl.es>
2019-04-24 12:45:00 -0700

    Fix method of setting default and url path

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,24 +2,26 @@
 """Creates and runs a Flask web app"""
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_text(text):
     return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
 def py_text(text="is cool"):
     return "Python {}".format(text.replace('_', ' '))
 

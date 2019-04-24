b30f6141e71888fa4627dd2402c5f181a47b325c
Wescott Sharples <wesc@ttsharpl.es>
2019-04-24 12:49:57 -0700

    Script checks returns if 'n' is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Creates and runs a Flask web app"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def py_text(text):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/n/<int:n>')
+def is_number(n):
+    return "{:d} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

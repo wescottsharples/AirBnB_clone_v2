23dd20606654b571bb5eb57f25e71780c729546e
Wescott Sharples <wesc@ttsharpl.es>
2019-04-24 13:56:07 -0700

    Script returns HTML with number's parity

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Creates and runs a Flask web app"""
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def py_text(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html_if_number(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_template/<int:n>')
+def html_parity(n):
+    parity = "even" if n % 2 == 0 else "odd"
+    return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
